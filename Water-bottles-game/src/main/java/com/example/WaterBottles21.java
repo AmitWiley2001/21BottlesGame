a4e4569d40d1312b5d92003e1cacd4de8acd8500
AmitWiley <49026501+AmitWiley2001@users.noreply.github.com>
2023-01-27 19:13:41 +1100

    Update WaterBottles21.java
    The temporary solution provided in the Question is removed now. Test case would fail but its correct.

--- Water-bottles-game/src/main/java/com/example/WaterBottles21.java
+++ Water-bottles-game/src/main/java/com/example/WaterBottles21.java
@@ -45,31 +45,14 @@ public class WaterBottles21 {
 			//   and return back -1
 			 
 			//YOUR CODE STARTS HERE
-		        try {
-					int num = this.scanner.nextInt();
-					
-					if(num >= 1 && num <= 4) {
-						return num;
-					 }
-					else {
-						//throw new InputMismatchException();
-						this.printStream.print("As per the Game Rules, please pick up bottles between 1 and 4 only, both inclusive");
-					}
-					
-				}catch(Exception e) {
-					//throw new InputMismatchException();
-					this.printStream.print("As per the Game Rules, please pick up bottles between 1 and 4 only, both inclusive");
-				}
-				return -1;
-                        //return -1;
+                        return -1;
 	                //YOUR CODE ENDS HERE	
 	 }
  
 	 
 	public int pickAfterUser(int pick1) {
 			//YOUR CODE STARTS HERE
-		        return 5-pick1;
-                        //return -1;
+                        return -1;
 	                //YOUR CODE ENDS HERE
 	}
 	 
